52d0dbdbdb0ca39523d94b0dd80be7d4acc9ca77
Bharathikannan Ramakrishnan <bharathikannan@live.com>
2021-03-31 00:33:28 +0530

    logic for API and comp
    logic for API and comp

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Discover from './Discover';
+import config from "../config";
 
 export default function Routes() {
-  // Here you'd return an array of routes
-  return <Discover />;
+  const [token, setToken] = useState("");
+  const [error, setError] = useState({});
+  const [newReleases, setNewReleases] = useState({});
+  const [playlists, setPlaylists] = useState({});
+  const [categories, setCategories] = useState({});
+  useEffect(() => {
+    const auth = btoa(`${config.api.clientId}:${config.api.clientSecret}`);
+    const body = new URLSearchParams();
+    body.append("grant_type", "client_credentials");
+    const getToken = async () => {
+      const res = await fetch(`${config.api.authUrl}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${auth}`,
+        },
+        body,
+        redirect: "follow",
+      });
+      const result = await res.json();
+      setToken(result.access_token);
+    };
+    getToken();
+  }, [error]);
+  useEffect(() => {
+    if (!token) return;
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const getAlbum = async (categories) => {
+      const res = await fetch(`${config.api.baseUrl}/browse/${categories}`, {
+        headers,
+      });
+      if (res.status === 401) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    };
+
+    const getData = async () => {
+      try {
+        const [newReleases, featured, categories] = await Promise.all([
+          getAlbum("new-releases"),
+          getAlbum("featured-playlists"),
+          getAlbum("categories"),
+        ]);
+        setNewReleases(newReleases.albums);
+        setPlaylists(featured.playlists);
+        setCategories(categories.categories);
+      } catch (error) {
+        if (error.message === "401") {
+          setError({});
+        }
+      }
+    };
+    getData();
+  }, [token]);
+  return ( <Discover
+    playlists={playlists.items ?? []}
+    newReleases={newReleases.items ?? []}
+    categories={categories.items ?? []}
+  />);
 }
