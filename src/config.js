91534d73574397436fd9575452dae4b711c27edb
Bharathikannan Ramakrishnan <bharathikannan@live.com>
2021-03-31 00:34:46 +0530

    Secret key / API key
    Secret key / API key

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: 'dca28481d3ce44bcb9c0c3186b378915',
+    clientSecret: 'ae9cf2e8b9ff418abf08f8cb8902f76e'
   }
 }
